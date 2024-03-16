fcb9a90a2d822d91326f11ef1ed35ea95db77cc7
wquadb <esavdeev@edu.hse.ru>
2024-03-17 02:45:38 +0300

    first task done, fifth is underway

--- taskA/main.cpp
+++ taskA/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <set>
+#include <vector>
 
 int main()
 {
-    
+    int n, k, val;
+    std::cin >> n >> k;
+    std::vector<int> vec;
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> val;
+        vec.push_back(val);
+    }
+    std::multiset<int> m(vec.begin(), vec.begin() + k);
+    std::cout << *m.begin() << "\n";
+    for (int i = k; i < n; i++)
+    {
+        m.erase(m.lower_bound(vec[i - k]));
+        m.insert(vec[i]);
+        std::cout << *m.begin() << "\n";
+    }
+    return 0;
 }
--- taskE/main.cpp
+++ taskE/main.cpp
@@ -1,6 +1,20 @@
+#include "main.h"
 #include <iostream>
 
-int main()
-{
-    
+int main() {
+    size_t m, n;
+    std::cin >> m >> n;
+    Minesweeper ms(m, n);
+
+    int minesCount;
+    std::cin >> minesCount;
+    while (minesCount-->0) {
+        size_t x, y;
+        std::cin >> x >> y;
+        ms.SetMine(x, y);
+    }
+
+    ms.CheckForMinesAround();
+
+    std::cout << ms;
 }
